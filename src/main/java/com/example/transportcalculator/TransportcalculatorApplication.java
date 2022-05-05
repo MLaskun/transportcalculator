36f62c0b5aaad59aafe0e9a01c239e1a132f2e7f
Mateusz Laska <mateo13311@gmail.com>
2022-05-05 20:01:12 +0200

    Spring initializer

---
+++ src/main/java/com/example/transportcalculator/TransportcalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.transportcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportcalculatorApplication.class, args);
+	}
+
+}
