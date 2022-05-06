3e75ce953e18fb077a091eeeb13e259b8a9b64c7
Mateusz Laska <mateo13311@gmail.com>
2022-05-06 02:21:45 +0200

    Update MeansOfTransportRepository.java

--- src/main/java/com/example/transportcalculator/domain/repository/MeansOfTransportRepository.java
+++ src/main/java/com/example/transportcalculator/domain/repository/MeansOfTransportRepository.java
@@ -2,6 +2,8 @@ package com.example.transportcalculator.domain.repository;
 
 import com.example.transportcalculator.domain.entity.MeansOfTransport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MeansOfTransportRepository extends JpaRepository<MeansOfTransport, Long> {
 }
